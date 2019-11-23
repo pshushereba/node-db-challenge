767cfbb2447e59cd035da087a44751374e81b975
Patrick Shushereba <pshushereba@gmail.com>
2019-11-23 11:34:38 -0800

    Finished self study questions.

--- projects/project-model.js
+++ projects/project-model.js
@@ -4,6 +4,37 @@ function getAllProjects() {
     return db('projects');
 }
 
+function addProject(project) {
+    return db('projects').insert(project)
+        .then((res) => {
+            const id = res[0]
+            return db('projects').where({id: id});
+        })
+}
+
+function getAllResources() {
+    return db('resources');
+}
+
+function addResource(resource) {
+    return db('resources').insert(resource)
+    .then((res) => {
+        const id = res[0]
+        return db('resources').where({id: id});
+    })
+}
+
+function getTasks(id) {
+    return db('tasks')
+    .join('projects', 'projects.id', 'tasks.project_id')
+    .select('tasks.id', 'projects.project_name', 'tasks.task_description', 'tasks.notes', 'tasks.completed')
+    .where({ 'projects.id': id });
+}
+
 module.exports = {
-    getAllProjects
+    getAllProjects,
+    addProject,
+    getAllResources,
+    addResource,
+    getTasks
 };
--- projects/project-router.js
+++ projects/project-router.js
@@ -4,6 +4,7 @@ const Projects = require('./project-model.js');
 
 const router = express.Router();
 
+// retrieving a list of projects.
 router.get('/', (req, res) => {
     Projects.getAllProjects()
         .then((projects) => {
@@ -11,6 +12,60 @@ router.get('/', (req, res) => {
         })
         .catch((err) => {
             res.status(500).json({message: "There was an error retrieving the projects"})
+        })
+})
+
+// adding projects.
+router.post('/', (req, res) => {
+    const projectInfo = req.body;
+    Projects.addProject(projectInfo)
+        .then((newProject) => {
+            res.status(201).json(newProject)
+        })
+        .catch((err) => {
+            res.status(500).json({message: "Failed to create new project"});
+        });
+});
+
+// adding resources.
+
+router.post('/resources', (req, res) => {
+    const resourceInfo = req.body;
+    Projects.addResource(resourceInfo)
+        .then((newResource) => {
+            res.status(201).json(newResource)
+        })
+        .catch((err) => {
+            res.status(500).json({message: "Failed to create new resource."});
+        });
+});
+
+
+// retrieving a list of resources.
+router.get('/resources', (req, res) => {
+    Projects.getAllResources()
+        .then((resources) => {
+            res.status(200).json(resources);
+        })
+        .catch((err) => {
+            res.status(500).json({message: "There was an error retrieving the resources"})
+        })
+})
+
+// adding tasks.
+
+
+// retrieving a list of tasks.
+
+router.get('/:id/tasks', (req, res) => {
+    const {id} = req.params;
+
+    Projects.getTasks(id)
+        .then((tasks) => {
+            res.status(200).json(tasks);
+        })
+        .catch((err) => {
+            res.status(500).json({message: "There was an error retrieving the tasks"})
         })
 })
 
